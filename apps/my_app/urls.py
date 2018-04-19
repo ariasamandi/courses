3756fda1bcbd84812dd167a0a0eb37131bef2747
Aria Samandi <ariasamandi@gmail.com>
2018-04-19 12:12:35 -0700

    courses

--- apps/my_app/urls.py
+++ apps/my_app/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
-    url(r'^$', views.index),     # This line has changed!
+    url(r'^$', views.index),
+    url(r'^add$', views.add),
+    url(r'^destroy/(?P<number>\d+)$', views.destroy),   
+    url(r'^remove/(?P<number>\d+)$', views.remove)     # This line has changed!
 ]
--- apps/my_app/views.py
+++ apps/my_app/views.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from models import *
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'my_app/index.html')
+    context = {
+        "Course" : Course.objects.all()
+    }
+    return render(request, 'my_app/index.html', context)
+def add(request):
+    Course.objects.create(course_name=request.POST['course_name'], description=request.POST['description'])
+    return redirect('/')
+def remove(request, number):
+    Course.objects.get(id=number).delete()
+    return redirect('/')
+def destroy(request, number):
+    context = {
+        "Course" : Course.objects.get(id=number)
+    }
+    return render(request, 'my_app/destroy.html', context)
